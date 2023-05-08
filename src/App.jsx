3328161fb35ad8f039197e002a49a5805f6c50fd
unaiccz <unaicc2003@gmail.com>
2023-05-08 13:29:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Brb from './components/Bar'
+
+function App() {
+  return (
+    <div>
+      <Brb/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Bar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {BarChart, ResponsiveContainer, } from 'recharts'
+import {Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+function Brb() {
+      const data = [
+        {name: 'John', age: 20, weight: 80},
+        {name: 'Jane', age: 21, weight: 70},
+        {name: 'Joe', age: 22, weight: 40},
+        {name: 'Jill', age: 23, weight: 120},
+    ]
+  return (
+    <div>
+      <h1 className='text-center'>Recharts</h1>
+{/* <create a grafic for the thata that i just puted */}
+      <ResponsiveContainer width="100%" aspect={2}>
+        <BarChart data={data} 
+        height={300}
+        width={500}
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}
+        >
+          <CartesianGrid strokeDasharray='4 1 2'/>
+          <XAxis dataKey='name'/>
+          <YAxis/>
+          <Tooltip/>
+          <Legend/>
+          <Bar dataKey='weight' fill='#8884d8'/>
+          <Bar dataKey='age' fill='#82ca9d'/>
+        </BarChart>
+      </ResponsiveContainer>
+      
+    </div>
+  )
+}
+
+export default Brb
